7ad38f6dabca9777d476d414fe7f93f866e31d6b
Lee Sewoo <dltpdn@gmail.com>
2019-11-29 16:14:25 +0900

    adding example moving sin/cos remap

--- 05.geometric_transform/remap_sin_cos.py
+++ 05.geometric_transform/remap_sin_cos.py
@@ -11,23 +11,20 @@ rows, cols = img.shape[:2]
 mapy, mapx = np.indices((rows, cols),dtype=np.float32)
 
 # sin, cos 함수를 적용한 변형 매핑 연산 ---②
+sinx = mapx + amp * np.sin(mapy/l)  
+cosy = mapy + amp * np.cos(mapx/l)
 
 # 영상 재매핑 ---③
-cnt = 0
-while True:
-    sinx = mapx + amp * np.sin(mapy/l)  
-    cosy = mapy + amp * np.cos(mapx/l)
+img_sinx=cv2.remap(img, sinx, mapy, cv2.INTER_LINEAR) # x축만 sin 곡선 적용
+img_cosy=cv2.remap(img, mapx, cosy, cv2.INTER_LINEAR) # y축만 cos 곡선 적용
+# x,y 축 모두 sin, cos 곡선 적용 및 외곽 영역 보정
+img_both=cv2.remap(img, sinx, cosy, cv2.INTER_LINEAR, None, cv2.BORDER_REPLICATE)
 
-    img_sinx=cv2.remap(img, sinx, mapy, cv2.INTER_LINEAR) # x축만 sin 곡선 적용
-    img_cosy=cv2.remap(img, mapx, cosy, cv2.INTER_LINEAR) # y축만 cos 곡선 적용
-    # x,y 축 모두 sin, cos 곡선 적용 및 외곽 영역 보정
-    img_both=cv2.remap(img, sinx, cosy, cv2.INTER_LINEAR, None, cv2.BORDER_REPLICATE)
-    cnt+=1
-    # 결과 출력 
-    cv2.imshow('origin', img)
-    cv2.imshow('sin x', img_sinx)
-    cv2.imshow('cos y', img_cosy)
-    cv2.imshow('sin cos', img_both)
+# 결과 출력 
+cv2.imshow('origin', img)
+cv2.imshow('sin x', img_sinx)
+cv2.imshow('cos y', img_cosy)
+cv2.imshow('sin cos', img_both)
 
-    if cv2.waitKey(100) != -1: break
+cv2.waitKey()
 cv2.destroyAllWindows()
